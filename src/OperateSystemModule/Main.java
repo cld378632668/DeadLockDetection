0e880581150f441201aaae233a2460b0a2c3364c
陈黎栋 <378632668@qq.com>
2019-12-31 17:47:46 +0800

    init

---
+++ src/OperateSystemModule/Main.java
@@ -0,0 +1,10 @@
+package OperateSystemModule;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JugdeDeadLockSingleResource jugdeDeadLockSingleResource = new JugdeDeadLockSingleResource();
+        jugdeDeadLockSingleResource.jugdeDeadLockSingleResource();
+    }
+}
